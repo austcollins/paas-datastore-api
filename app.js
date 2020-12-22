2086465655bb6b70373bdd1bd41c8d2af9f4cea2
Austin <47179017+austcollins@users.noreply.github.com>
2020-12-22 11:51:02 +0000

    Express Installed

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('static', { extensions: ['html'] }));
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, (err) => {
+  if (err) console.log('error', err);
+  else console.log(`app listening on port ${port}`);
+});
