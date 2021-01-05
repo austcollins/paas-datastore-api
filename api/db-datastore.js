879ad50b548f958566e7f9695143250cb14b1f52
Austin <47179017+austcollins@users.noreply.github.com>
2021-01-05 12:20:03 +0000

    cond 1

--- api/db-datastore.js
+++ api/db-datastore.js
@@ -17,8 +17,7 @@ module.exports.list = async () => {
 
 module.exports.get = async (id) => {
   const [data] = await datastore.get(key(id));
-  if (data && data.val) return data.val;
-  return '';
+  return (data === undefined || data.length == 0) ? 0 : data.val; 
 };
 
 module.exports.put = (id, val) => {
